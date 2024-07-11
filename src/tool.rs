ec9659508859ec861e36a6c45ce89a392398bae1
Kevin F. Konrad <kevin.konrad@skillbyte.de>
2024-07-11 22:10:51 +0200

    implement match-subdirectories feature

--- src/tool.rs
+++ src/tool.rs
@@ -2,28 +2,37 @@ use serde_toml_merge::merge_into_table;
 use std::env;
 use toml::Table;
 
-use crate::{conf, path};
+use crate::{
+    conf::{self, Inject},
+    path,
+};
 
 pub fn handle(tool: conf::Tool) {
     let current_dir = env::current_dir().unwrap();
     let mut merged_config: Table = path::read_toml(&tool.rootconfig.0);
     for subconfig in tool.subconfigs {
-        if path::is_subdir(&subconfig.directory.0, &current_dir) {
+        if (subconfig.match_subdirectories && path::is_subdir(&subconfig.directory.0, &current_dir))
+            || subconfig.directory.0 == current_dir
+        {
             let additional_config: Table = path::read_toml(&subconfig.config.0);
             merge_into_table(&mut merged_config, additional_config).unwrap();
         }
     }
-    for injection in tool.inject {
-        path::write_toml(&injection.location, &merged_config);
-        match injection.inject_type {
-            conf::InjectType::Env => {
-                println!(
-                    "export {}={:#?}",
-                    injection.name.unwrap(),
-                    path::normalize(&injection.location).to_string_lossy()
-                )
-            }
-            conf::InjectType::File => {}
-        }
+    for inject in tool.inject {
+        perform_injection(inject, &merged_config);
     }
+}
+
+fn perform_injection(inject: Inject, table: &Table) {
+    path::write_toml(&inject.location, &table);
+    match inject.inject_type {
+        conf::InjectType::Env => {
+            println!(
+                "export {}={:#?}",
+                inject.name.unwrap(),
+                path::normalize(&inject.location).to_string_lossy()
+            )
+        }
+        conf::InjectType::File => {}
+    };
 }
